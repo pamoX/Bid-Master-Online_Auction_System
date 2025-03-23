3264ed7b581c06aec2871020421ab416f5821534
Pamodini Perera <IT23423688@my.sliit.lk>
2025-03-23 14:54:08 +0530

    create frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AboutUs from "./Components/AboutUs/AboutUs";
+
+function App() {
+  return (
+    <div>
+      <AboutUs></AboutUs>
+    </div>
+  );
+}
+
+export default App;
