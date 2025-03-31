30274f9de8cdb5afefe9217b9e415b2112744fde
Pamodini Perera <IT23423688@my.sliit.lk>
2025-04-01 00:24:43 +0530

    edit ReportModel

--- Backend/Model/ReportModel.js
+++ Backend/Model/ReportModel.js
@@ -1,6 +1,10 @@
+//add mongoose
 const mongoose = require("mongoose");
+
+//assign mongoose to schema
 const Schema = mongoose.Schema;
 
+//implementing a function to connnet data  that we are with this schema
 const reportSchema = new Schema({
     ReportName:{
         type:String,//datatype
@@ -16,6 +20,7 @@ const reportSchema = new Schema({
     }
 });
 
+//export the model that we created
 module.exports = mongoose.model(
     "ReportModel",//file name
     reportSchema//function name
