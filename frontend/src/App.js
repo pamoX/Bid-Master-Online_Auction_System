c64ab34c6dffd6ddfc36791ce8b1ed41a9c27cd2
Pamodini Perera <IT23423688@my.sliit.lk>
2025-05-09 09:24:35 +0530

    add paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,9 @@ import { Route , Routes} from "react-router-dom";
 import "./App.css";
 import AboutUs from "./Components/AboutUs/AboutUs";
 import ReportedItems from "./Components/ReportedItems/ReportedItems";
-import InspectionReport from "./Components/InspectionReport/InspectionReport";
+import DisplayItems from "./Components/DisplayItems/DisplayItems";
+import ItemForm from "./Components/ItemForm/ItemForm";
+import ItemsGallery from "./Components/ItemsGallery/ItemsGallery";
 import RejectedItems from "./Components/RejectedItems/RejectedItems";
 import Home from "./Components/Home/Home";
 import AddReport from "./Components/AddReport/AddReport";
@@ -20,10 +22,14 @@ function App() {
           <Route path="/about-us" element={<AboutUs/>}/>
           <Route path="/dashboard" element={<Dashboard/>}/>
           <Route path="/reject-items" element={<RejectedItems/>}/>
+          <Route path="/rejected-items/:id" element={<RejectedItems />} />
+          <Route path="/display-items" element={<DisplayItems/>}/>
+          <Route path="/item-form/:id" element={<ItemForm />} />
+          <Route path="/items-gallery" element={<ItemsGallery />} />
           <Route path="/flagged-items" element={<ReportedItems/>}/>
           <Route path="/flagged-items/:_id" element={<UpdateReport/>}/>
           <Route path="/add-report" element={<AddReport/>}/>
-          <Route path="/inspection-report" element={<InspectionReport/>}/>
+          <Route path="/items-gallery" element={<ItemsGallery/>}/>
         </Routes>
       </React.Fragment>
 
