bd094cbd880802f763fab4c5e29c46ae30d426ad
Poornima Liyanage <n.poornimaliyanage@gmail.com>
2025-05-14 15:29:31 +0530

    jj
    just

--- Backend/Model/AuctionModel.js
+++ Backend/Model/AuctionModel.js
@@ -4,7 +4,7 @@ const auctionSchema = new mongoose.Schema({
   name: { type: String, required: true },
   biddingEndTime: { type: Date, required: true },
   winnerNotified: { type: Boolean, default:false },
-  // Add other fields as needed
+  
 });
 
-module.exports = mongoose.model('Auction', auctionSchema, 'items');
+module.exports = mongoose.model('Auction', auctionSchema, 'items');// auctionSchema
